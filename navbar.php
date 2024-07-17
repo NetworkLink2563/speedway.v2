30c4528ca50b694052e21524d4a4b1f8fe0568b5
networklink.bank <>
2024-07-17 14:27:06 +0700

    no message

--- navbar.php
+++ navbar.php
@@ -52,7 +52,7 @@
         justify-content: center;
         align-items: center;
         width: 20%;
-        text-align: center;
+        text-align: right;
     }
 
     .msg-right p{
@@ -130,6 +130,10 @@
     font-size: 0.8rem;
 }
 
+p {
+  font-size: .8rem;
+}
+</style>
 </style>
 
     <!-- navbar  start-->
@@ -141,7 +145,7 @@
     Department of Hightways</p>
 </div>
         
-        <h4>VMS : Variable Message Sign : PC1</h4>
+        <h4></h4>
         <div class="msg-right">
         <!-- <p>(test3@test.com)<br>
         วันพฤหัส ที่ 11 เดือน กรกฏาคม พ.ศ. 2567<br>
@@ -237,11 +241,47 @@
         </li>
       </ul>
       
+      <div class="time-duration navbar-nav" style="border: none; text-align: center;  margin-left: .5rem; opacity: .8">
+        <span >เวลาเข้าทำงาน : </span>
+      <span id="demo" style="display: inline-block;padding: 0rem .5rem;width: 140px; text-align: left;"></span>
+      </div>
+
+
     </div>
   </div>
 </nav>
-
 </nav>
      <!-- navbar end-->
+
+     <script>
+// Set the date we're counting down to
+var countDownDate = new Date("Jan 5, 2030 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
+</script>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
