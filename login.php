c15500a09ed59c2ad33efe5ed547b5a978da7cdd
networklink.bank <>
2024-08-06 10:23:04 +0700

    no message

--- login.php
+++ login.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 <?php
 header('Content-type: text/html; charset=utf-8');
@@ -15,22 +14,4 @@ if(isset($_POST['load']) and $_POST['load']=='0001'){
      echo $QF['XVShfCode'];
   }
 
-=======
-
-<?php
-header('Content-type: text/html; charset=utf-8');
-ob_start();
-session_start();
-
-if(isset($_POST['load']) and $_POST['load']=='0001'){
-    $username=$_POST["userName"];
-    $password=$_POST["password"];
-    include "config_NWL.php";
-     $Q="SELECT XVShfCode FROM [NWL_SpeedWayTest2].[dbo].[TMstMUser] WHERE XVUsrCode='$username'";
-     $Qk=sqlsrv_query($conn, $Q);
-     $QF=sqlsrv_fetch_array($Qk, SQLSRV_FETCH_ASSOC);
-     echo $QF['XVShfCode'];
-  }
-
->>>>>>> origin/main
 ?>
