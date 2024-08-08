25c3f81f0d5f41fdbbb8bd8c59f2af76f1163dc8
b-werayut <l2omance1992@gmail.com>
2024-08-08 10:37:53 +0700

    Update usermenu.php

--- usermenu.php
+++ usermenu.php
@@ -448,7 +448,7 @@ body {
 
 
 <div style=" text-align: center; padding: 1rem; border-bottom: 3px double #cccc; margin: .4rem;">
-            <img src="http://43.229.151.103/speedway/img/icon/setting.png" height="25" alt="Responsive image"> ผู้ใช้งาน
+            <img src="/speedway/img/icon/setting.png" height="25" alt="Responsive image"> ผู้ใช้งาน
         </div>
 
 
