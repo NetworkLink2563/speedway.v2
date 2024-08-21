e5358ce7fe758d08fd3fdf5ea52fec07fc0a5ea7
networklink.bank <>
2024-08-21 17:28:19 +0700

    no message

--- index.php
+++ index.php
@@ -69,8 +69,8 @@ function ThDate()
 <!-- Bootstrap core JavaScript
 ================================================== -->
 <!-- Placed at the end of the document so the pages load faster -->
-<script src="dist/js/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+<script src="./dist/js/jquery-3.2.1.slim.min.js"></script>
+<script src="./dist/js/jquery-3.7.1.min.js"></script>
 
 
 <script type="text/javascript">
