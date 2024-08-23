5fcd58e352681149ea1b73b8869ec4cfcec72efa
networklink.bank <>
2024-08-23 14:30:39 +0700

    no message

--- messagepublicrelationsplay.php
+++ messagepublicrelationsplay.php
@@ -806,7 +806,8 @@ new DataTable('#VMSTable', {
         zeroRecords: '" ไม่พบข้อมูลที่ค้นหา "',
         info: 'แสดง _END_ รายการ จากทั้งหมด _MAX_ รายการ',
         infoFiltered: '',
-        infoEmpty: 'ไม่พบรายการ'
+        infoEmpty: 'ไม่พบรายการ',
+        emptyTable: '" ไม่มีข้อมูลในตาราง "'
     }
              
 });
