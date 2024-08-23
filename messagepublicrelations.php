aa8f0a1c642b9617d32d4db265bf29e65b7dc939
networklink.bank <>
2024-08-23 11:40:31 +0700

    no message

--- messagepublicrelations.php
+++ messagepublicrelations.php
@@ -1287,7 +1287,7 @@ function examplesms(url, h, w, vmsmame) {
 
     document.getElementById("Example_Title").innerText = vmsmame + " ขนาด กว้าง=" + w + " สูง=" + h;
 
-    document.getElementById("iframe").width = parseInt(w);
+    document.getElementById("iframe").width = parseInt(980);
     document.getElementById("iframe").height = parseInt(520);
     document.getElementById("iframe").src = url;
 
